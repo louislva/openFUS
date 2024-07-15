2a255c61b4a85b0a142a15aede5858858d652051
Louis Arge <louislva@gmail.com>
2024-07-15 20:03:29 +0200

    think this is roughly righr

--- matlab/plot3d.m
+++ matlab/plot3d.m
@@ -6,22 +6,39 @@ voxelSize = 50; % Adjust as needed
 voxelMm = 1;
 % voxelVolume = zeros(voxelSize, voxelSize, voxelSize);
 
-function sphereVoxels = createSphere(radius)
-    size = ceil(radius * 2);
-    sphereVoxels = zeros(size, size, size);
-    [X, Y, Z] = ndgrid(1:size, 1:size, 1:size);
-    sphere = sqrt((X - radius).^2 + (Y - radius).^2 + (Z - radius).^2) <= radius;
-    sphereVoxels(sphere) = 1;
+% function sphereVoxels = createSphere(radius)
+%     size = ceil(radius * 2);
+%     sphereVoxels = zeros(size, size, size);
+%     [X, Y, Z] = ndgrid(1:size, 1:size, 1:size);
+%     sphere = sqrt((X - radius).^2 + (Y - radius).^2 + (Z - radius).^2) <= radius;
+%     sphereVoxels(sphere) = 1;
+% end
+
+function cylinderVoxels = createCylinder(diameter, depth)
+    cylinderVoxels = zeros(diameter, diameter, depth);
+    [X, Y, Z] = ndgrid(1:diameter, 1:diameter, 1:depth);
+    cylinder = sqrt((X - diameter/2).^2 + (Y - diameter/2).^2) <= diameter/2;
+    cylinderVoxels(cylinder) = 1;
 end
 
-function lensVoxels = createLens(width, height, radius)
-    lensVoxels = ones(width, width, height);
-    [X, Y, Z] = ndgrid(1:width, 1:width, 1:height);
-    sphere = sqrt((X - width/2).^2 + (Y - width/2).^2 + (height - Z).^2) <= radius;
+function lensVoxels = createLens(diameter, depth, radius, padding)
+    lensVoxels = zeros(diameter, diameter, depth);
+    % Add cylinder
+    cylinder = createCylinder(diameter, depth);
+    lensVoxels = lensVoxels + cylinder;
+
+    % Subtract sphere
+    [X, Y, Z] = ndgrid(1:diameter, 1:diameter, 1:depth);
+    sphere = sqrt((X - diameter/2).^2 + (Y - diameter/2).^2 + (Z - radius - padding).^2) <= radius;
     lensVoxels(sphere) = 0;
 end
 
-voxelVolume = createLens(10, 10, 5);
+function lensVoxels = createLensImplicit(diameter, radius, padding)
+    depth = sqrt(radius^2 - (diameter / 2)^2) + padding;
+    lensVoxels = createLens(diameter, ceil(depth), radius, padding + 1);
+end
+
+voxelVolume = createLensImplicit(50, 75, 2);
 
 % Visualize the sphere
 figure;
