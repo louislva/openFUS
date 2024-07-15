27d336d11ff0abb710c9c871dc28f3a9625000ef
Louis Arge <louislva@gmail.com>
2024-07-15 21:21:19 +0200

    BUT WHERE DOES SOURCE COME FROM

--- matlab/simulateTransducerAndLens.m
+++ matlab/simulateTransducerAndLens.m
@@ -1,5 +1,4 @@
-% Lens utils
-
+% LENS UTILITY FUNCTIONS
 function cylinderVoxels = createCylinder(diameter, depth)
     cylinderVoxels = zeros(diameter, diameter, depth);
     [X, Y, Z] = ndgrid(1:diameter, 1:diameter, 1:depth);
@@ -51,31 +50,26 @@ function notVoxels = invertVoxels(originalVoxels)
     notVoxels = 1 - originalVoxels;
 end
 
-% Constants
-
+% CONSTANTS
 WATER_SPEED = 1500;
 WATER_DENSITY = 1000;
 
-PZT_SPEED = 4000;
+PZT_SPEED = 4000; % Transducer material, PZT-5H
 PZT_DENSITY = 7800; % (source: https://www.steminc.com/piezo/PZ_property.asp)
 
-% Lens material, polypropylene
-PP_SPEED = 2565;
+PP_SPEED = 2565; % Lens material, polypropylene
 PP_DENSITY = 920;
 
-% Silver epoxy
-SE_SPEED = 2548;
+SE_SPEED = 2548; % Silver epoxy, impedence transformer + electrode
 SE_DENSITY = 3222;
 
+% Simulation constants (derived from our constraints)
 c_min = WATER_SPEED;
 c_max = PZT_SPEED;
 f_max = 500000;
 min_wave_length = c_min / f_max;
 global grid_size;
 grid_size = min_wave_length / 4;
-
-% Simulation
-speed_multiplier = 1;
 
 function voxelLength = mmToVoxelLength(mm)
     global grid_size;
@@ -85,9 +79,9 @@ end
 Nx = 100;   % number of grid points in the x direction
 Ny = 100;   % number of grid points in the y direction
 Nz = 100;   % number of grid points in the z direction
-dx = grid_size * speed_multiplier;   % grid point spacing in the x direction [m]
-dy = grid_size * speed_multiplier;   % grid point spacing in the y direction [m]
-dz = grid_size * speed_multiplier;   % grid point spacing in the z direction [m]
+dx = grid_size;   % grid point spacing in the x direction [m]
+dy = grid_size;   % grid point spacing in the y direction [m]
+dz = grid_size;   % grid point spacing in the z direction [m]
 kgrid = makeGrid(Nx, dx, Ny, dy, Nz, dz);
 
 % Define the time array for the kgrid
@@ -101,34 +95,57 @@ kgrid.t_array = (0:Nt-1) * dt; % time array
 fprintf('Time step (dt): %e seconds\n', dt);
 fprintf('Total time: %e seconds\n', kgrid.t_array(end));
 
-% Objects
+% DEFINE OBJECTS
+
+% General transducer thing
+T_x = 50;
+T_radius = mmToVoxelLength(30 / 2);
+T_x0 = floor(T_x - T_radius);
+T_x1 = ceil(T_x + T_radius);
 
 % Source, PZT
-S_x = 50;
-S_radius = mmToVoxelLength(30 / 2);
 S_depth = mmToVoxelLength(4.2);
-S_z = 1;
+S_z0 = 1;
+S_z1 = ceil(S_z0 + S_depth);
+transducerRaw = createCylinder(T_radius * 2, ceil(S_depth));
+transducer = makeBig(Nx, Ny, Nz, transducerRaw, 0, 0, S_z0);
 
-S_x0 = floor(S_x - S_radius);
-S_x1 = ceil(S_x + S_radius);
-S_z0 = S_z;
-S_z1 = ceil(S_z + S_depth);
+% Silver Epoxy
+E_depth = round(mmToVoxelLength((SE_SPEED / 500000 / 4) * 1000));
+fprintf('E_depth: %f\n', E_depth);
+silverEpoxyRaw = createCylinder(T_radius * 2, E_depth);
+silverEpoxy = makeBig(Nx, Ny, Nz, silverEpoxyRaw, 0, 0, S_z1);
+
+E_z0 = S_z1 + 0;
+E_z1 = ceil(E_z0 + E_depth);
 
 % Lens, PP
-L_z = S_z1;
-
-annotation.mask = zeros(Nx, Ny, Nz);
-
-% Define the properties of the first material
-medium.sound_speed = WATER_SPEED * ones(Nx, Ny, Nz);  % [m/s]
-medium.density = WATER_DENSITY * ones(Nx, Ny, Nz);      % [kg/m^3]
-
-% Define the region for the lens
 lensDiameter = mmToVoxelLength(30);
 lensRadius = mmToVoxelLength(22.84);
 lensPadding = mmToVoxelLength(1);
-lens = makeBig(Nx, Ny, Nz, createLens(lensDiameter, lensRadius, lensPadding), 0, 0, L_z + 1);
+lensRaw = createLens(lensDiameter, lensRadius, lensPadding);
+L_z = E_z1 + 0;
+L_z1 = ceil(L_z + size(lensRaw, 3));
+lens = makeBig(Nx, Ny, Nz, lensRaw, 0, 0, L_z + 1);
 
+% INSERT OBJECTS
+annotation.mask = zeros(Nx, Ny, Nz);
+medium.sound_speed = WATER_SPEED * ones(Nx, Ny, Nz);  % [m/s]
+medium.density = WATER_DENSITY * ones(Nx, Ny, Nz);      % [kg/m^3]
+
+% Transducer
+% medium.sound_speed = invertVoxels(transducer) .* medium.sound_speed + transducer .* PZT_SPEED;
+% medium.density = invertVoxels(transducer) .* medium.density + transducer .* PZT_DENSITY;
+% source.p_mask = transducer;
+% annotation.mask = annotation.mask + transducer;
+
+% Silver epoxy
+medium.sound_speed = invertVoxels(silverEpoxy) .* medium.sound_speed + silverEpoxy .* SE_SPEED;
+medium.density = invertVoxels(silverEpoxy) .* medium.density + silverEpoxy .* SE_DENSITY;
+
+% Lens
+medium.sound_speed = invertVoxels(lens) .* medium.sound_speed + lens .* PP_SPEED;
+medium.density = invertVoxels(lens) .* medium.density + lens .* PP_DENSITY;
 annotation.mask = annotation.mask + lens;
 
 % region_x = floor((Nx - lensDiameter) / 2):floor((Nx + lensDiameter) / 2);
@@ -136,19 +153,12 @@ annotation.mask = annotation.mask + lens;
 % region_z = floor((Nz - lensDiameter) / 2):floor((Nz + lensDiameter) / 2);
 % annotation.mask(region_x, region_y, region_z) = 1;
 
-medium.sound_speed = invertVoxels(lens) * WATER_SPEED + lens * PP_SPEED;
-medium.density = invertVoxels(lens) * WATER_DENSITY + lens * PP_DENSITY;
 
 % Define a time-varying sinusoidal source
-source_freq = 500000; % 1 MHz
+source_freq = 500000; % 500 kHz
 source_mag = 1; % magnitude of the source
 t_array = kgrid.t_array; % time array from the kgrid
 source.p = source_mag * sin(2 * pi * source_freq * t_array);
-
-% Define source location
-source.p_mask = zeros(Nx, Ny, Nz);
-source.p_mask(S_x0:S_x1, S_x0:S_x1, S_z0:S_z1) = 1;
-annotation.mask(S_x0:S_x1, S_x0:S_x1, S_z0:S_z1) = 1;
 
 sensor.mask = zeros(Nx, Ny, Nz);
 sensor.mask(1, 1, 1) = 1; % example of a single point sensor
