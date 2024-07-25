99c145f0900a2c6d7547b9330d1f8079da938feb
Louis Arge <louislva@gmail.com>
2024-07-26 00:34:09 +0200

    We can do measurements

--- aruco/aruco.py
+++ aruco/aruco.py
@@ -15,6 +15,9 @@ def track(camera_matrix, dist_coeffs):
 
     # ArucoDetector is a class that is used to detect ArUco markers in an image
     detector = aruco.ArucoDetector(aruco_dict, aruco_parameters)
+    
+    measurement = None
+    last_translation = None
 
     while True:
         # Capture frame-by-frame
@@ -27,6 +30,7 @@ def track(camera_matrix, dist_coeffs):
 
         # Detect ArUco markers
         corners, ids, rejected = detector.detectMarkers(gray)
+        
 
         # If markers are detected
         if ids is not None:
@@ -34,32 +38,50 @@ def track(camera_matrix, dist_coeffs):
             aruco.drawDetectedMarkers(frame, corners, ids)
 
             # Estimate pose of each marker
-            rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, 0.05, camera_matrix, dist_coeffs)
+            size = 0.0355
+            # size = 0.047
+            rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, size, camera_matrix, dist_coeffs)
 
             for i in range(len(ids)):
-                print(f"Marker {ids[i]} at {tvecs[i]}")
+                tvec = np.array(tvecs[i]).round(2)
+                last_translation = tvec
+                rvec = np.array(rvecs[i])
+                rvec_deg = np.degrees(rvec).round(0)
+                if measurement is None:
+                    print(f"Marker {ids[i]} at {tvec} // {rvec_deg}")
 
         # Display the resulting frame
+        frame = cv2.flip(frame, 1)
         cv2.imshow('ArUco Marker Tracking', frame)
 
         # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        key = cv2.waitKey(1)
+        if key == ord('q'):
             break
+        elif key == ord('m'):
+            measurement = last_translation
+            print(f"Measurement: {measurement}")
+        elif key == ord('n'):
+            measurement = None
+        
+        if measurement is not None:
+            distance_to_measurement = (np.linalg.norm(measurement - last_translation) * 100).round(2)
+            print(f"Distance to measurement: {distance_to_measurement}cm")
 
     # Release the capture and close windows
     cap.release()
     cv2.destroyAllWindows()
 
-def save_markers():
+def save_markers(padding=56, size=400):
     # Save markers
     aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
     os.makedirs("markers", exist_ok=True)
     for i in range(250):
         if os.path.exists(f"markers/marker_{i}.png"):
             continue
-        img = aruco.generateImageMarker(aruco_dict, i, 400)
+        img = aruco.generateImageMarker(aruco_dict, i, size)
         # add 56 padding each side white
-        img = cv2.copyMakeBorder(img, 56, 56, 56, 56, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+        img = cv2.copyMakeBorder(img, padding, padding, padding, padding, cv2.BORDER_CONSTANT, value=[255, 255, 255])
         cv2.imwrite(f"markers/marker_{i}.png", img)
 
 def calibrate_camera():
@@ -110,11 +132,11 @@ def calibrate_camera():
     return mtx, dist
 
 def main():
-    save_markers()
+    save_markers(padding=0, size=512)
     # calibrate_camera()
-    camera_matrix = np.load("camera_matrix.npy")
-    dist_coeffs = np.load("dist_coeffs.npy")
-    track(camera_matrix, dist_coeffs)
+    # camera_matrix = np.load("camera_matrix.npy")
+    # dist_coeffs = np.load("dist_coeffs.npy")
+    # track(camera_matrix, dist_coeffs)
 
 if __name__ == '__main__':
     main()
