820be0d560fedcdc01f76662ff4862da4212eff5
Louis Arge <louislva@gmail.com>
2024-09-27 14:21:34 -0700

    make stuff happen

---
+++ neuronavigation/app/main.py
@@ -0,0 +1,86 @@
+import os
+import numpy as np
+import pydicom
+import nibabel as nib
+import pyvista as pv
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+
+# print(dir(pv))
+# exit()
+
+class MRIViewer:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.image_3d = self.load_data()
+    
+    def load_data(self):
+        if os.path.isdir(self.file_path):
+            return self.load_dicom_series(self.file_path)
+        elif self.file_path.endswith('.nii') or self.file_path.endswith('.nii.gz'):
+            return self.load_nifti_file(self.file_path)
+        else:
+            raise ValueError("Unsupported file format")
+    
+    def load_dicom_series(self, directory):
+        slices = []
+        for filename in os.listdir(directory):
+            if filename.endswith('.dcm'):
+                ds = pydicom.dcmread(os.path.join(directory, filename))
+                slices.append(ds)
+        slices.sort(key=lambda x: float(x.ImagePositionPatient[2]))
+        image_3d = np.stack([s.pixel_array for s in slices], axis=-1)
+        return image_3d
+
+    def load_nifti_file(self, file_path):
+        nifti_img = nib.load(file_path)
+        image_3d = nifti_img.get_fdata()
+        return image_3d
+
+    def display_slice(self, slice_index):
+        plt.imshow(self.image_3d[:, :, slice_index], cmap='gray')
+        plt.axis('off')
+        plt.show()
+    
+    def interactive_slicing(self):
+        max_index = self.image_3d.shape[2] - 1
+        fig, ax = plt.subplots()
+        plt.subplots_adjust(bottom=0.25)
+        slice_index = max_index // 2
+        l = ax.imshow(self.image_3d[:, :, slice_index], cmap='gray')
+        ax_slice = plt.axes([0.25, 0.1, 0.65, 0.03])
+        slice_slider = Slider(ax_slice, 'Slice', 0, max_index, valinit=slice_index, valfmt='%0.0f')
+
+        def update(val):
+            idx = int(slice_slider.val)
+            l.set_data(self.image_3d[:, :, idx])
+            fig.canvas.draw_idle()
+
+        slice_slider.on_changed(update)
+        plt.show()
+    
+    def visualize_3d(self):
+        grid = pv.RectilinearGrid()
+        x = np.arange(0, self.image_3d.shape[0])
+        y = np.arange(0, self.image_3d.shape[1])
+        z = np.arange(0, self.image_3d.shape[2])
+        grid = pv.RectilinearGrid(x, y, z)
+        grid.point_data["values"] = self.image_3d.flatten(order="F")
+        plotter = pv.Plotter()
+        opacity = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]  # Adjust opacity mapping
+        plotter.add_volume(grid, cmap='viridis', opacity=opacity)
+        plotter.show()
+
+# Usage example:
+# Provide the path to a directory containing DICOM files or a NIfTI file
+viewer = MRIViewer('healthy-t1.nii')
+
+# # Display a single 2D slice
+# slice_index = 50  # Change this index to see different slices
+# viewer.display_slice(slice_index)
+
+# Interactive slicing with a slider
+viewer.interactive_slicing()
+
+# Interactive 3D volume visualization with rotation support
+viewer.visualize_3d()
